4e92c0213f35c608a5303722e27ae1e38988f04f
Samuel Soto <108969323+samucodesh@users.noreply.github.com>
2024-10-06 15:09:40 -0500

    Update idx-template.nix

--- idx-template/idx-template.nix
+++ idx-template/idx-template.nix
@@ -12,6 +12,8 @@
 
     # Remove the template files themselves and any connection to the template's
     # Git repository
-    rm -rf "$out/.git" "$out/idx-template".{nix,json}
+    rm -rf "$out/.git" "$out/idx-template".{nix,json} "$out/.github"
+
+    rm README.md
   '';
 }
