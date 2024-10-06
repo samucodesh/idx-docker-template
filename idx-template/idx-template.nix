f2983599ea1eb22b63206dc1c17175ef3a553ce4
Samuel Soto <108969323+samucodesh@users.noreply.github.com>
2024-10-06 15:03:39 -0500

    Revert "fix: Removed README.md file when creating environment"

--- idx-template/idx-template.nix
+++ idx-template/idx-template.nix
@@ -13,7 +13,5 @@
     # Remove the template files themselves and any connection to the template's
     # Git repository
     rm -rf "$out/.git" "$out/idx-template".{nix,json}
-
-    rm README.md
   '';
 }
